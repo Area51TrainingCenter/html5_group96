4d202e707527c8ff513c8dd4af34543ceff418a4
Raymi Saldomando <raymi15@gmail.com>
2017-10-30 20:59:30 -0500

    clase_19

--- modulo3/clase_19/ejercicios-alumnos/Ciclos-Ejercicio/script.js
+++ modulo3/clase_19/ejercicios-alumnos/Ciclos-Ejercicio/script.js
@@ -1,7 +1,7 @@
 /* 
   Ejercicio Bolas de Colores!
 
-  El programa que tienes a continuación se trata de crear divs con diferentes colores que se generan de manera aleatoria.
+  El programa que tienes a continuación se trata de crear divs con diferentes colores que se cargan de manera aleatoria.
 
   La forma en como está escrito el código es ineficiente. Utiliza lo aprendido de ciclos para hacer este código más corto y simple.
 
@@ -11,15 +11,35 @@
 var divColor = '';
 var rojo, verde, azul, colorRGB;
 
+rojo = Math.floor(Math.random() * 256);
+verde = Math.floor(Math.random() * 256);
+azul = Math.floor(Math.random() * 256);
+colorRGB = 'rgb(' + rojo + ',' + verde + ',' + azul + ')';
+divColor += '<div class="colores" style="background-color:' + colorRGB + '"></div>';
 
-// Resolviendo el ejercicio con ciclos
+rojo = Math.floor(Math.random() * 256);
+verde = Math.floor(Math.random() * 256);
+azul = Math.floor(Math.random() * 256);
+colorRGB = 'rgb(' + rojo + ',' + verde + ',' + azul + ')';
+divColor += '<div class="colores" style="background-color:' + colorRGB + '"></div>';
 
-for( var i = 1; i <= 100; i++ ){
-	rojo = Math.floor(Math.random() * 256);
-	verde = Math.floor(Math.random() * 256);
-	azul = Math.floor(Math.random() * 256);
-	colorRGB = 'rgb(' + rojo + ',' + verde + ',' + azul + ')';
-	divColor += '<div class="colores" style="background-color:' + colorRGB + '"></div>';
-}
+rojo = Math.floor(Math.random() * 256);
+verde = Math.floor(Math.random() * 256);
+azul = Math.floor(Math.random() * 256);
+colorRGB = 'rgb(' + rojo + ',' + verde + ',' + azul + ')';
+divColor += '<div class="colores" style="background-color:' + colorRGB + '"></div>';
+
+rojo = Math.floor(Math.random() * 256);
+verde = Math.floor(Math.random() * 256);
+azul = Math.floor(Math.random() * 256);
+colorRGB = 'rgb(' + rojo + ',' + verde + ',' + azul + ')';
+divColor += '<div class="colores" style="background-color:' + colorRGB + '"></div>';
+
+rojo = Math.floor(Math.random() * 256);
+verde = Math.floor(Math.random() * 256);
+azul = Math.floor(Math.random() * 256);
+colorRGB = 'rgb(' + rojo + ',' + verde + ',' + azul + ')';
+divColor += '<div class="colores" style="background-color:' + colorRGB + '"></div>';
 
 document.write(divColor);
+
